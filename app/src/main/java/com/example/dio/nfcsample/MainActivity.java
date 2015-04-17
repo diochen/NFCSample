b153d9f75c4e72236239f14770e2afd4ff73c111
diochen <diomoto99@gmail.com>
2015-04-17 14:20:24 +0800

    read tag id (serial number)

--- app/src/main/java/com/example/dio/nfcsample/MainActivity.java
+++ app/src/main/java/com/example/dio/nfcsample/MainActivity.java
@@ -1,17 +1,42 @@
 package com.example.dio.nfcsample;
 
+import android.app.Activity;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.util.Log;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements CardReader.TagCallback{
+    // for log
+    public static final int INFO = android.util.Log.INFO;
+    public static final String TAG = "CardReaderActivity";
+
+
+    // Recommend NfcAdapter flags for reading from other Android devices. Indicates that this
+    // activity is interested in NFC-A devices (including other Android devices), and that the
+    // system should not check for the presence of NDEF-formatted data (e.g. Android Beam).
+    public static int READER_FLAGS =
+            NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK;
+    public CardReader mCardReader;
+    private TextView mSerialNumView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSerialNumView = (TextView) findViewById(R.id.serialNumText);
+        mSerialNumView.setText("");
+
+
+        mCardReader = new CardReader(this);
+        // Disable Android Beam and register our card reader callback
+        enableReaderMode();
     }
 
 
@@ -35,5 +60,49 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableReaderMode();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        enableReaderMode();
+        mSerialNumView.setText("");
+    }
+
+    private void enableReaderMode() {
+        Log.i(TAG, "Enabling reader mode");
+        Activity activity = this;//getActivity();
+        NfcAdapter nfc = NfcAdapter.getDefaultAdapter(activity);
+        if (nfc != null) {
+            nfc.enableReaderMode(activity, mCardReader, READER_FLAGS, null);
+        }
+    }
+
+    private void disableReaderMode() {
+        Log.i(TAG, "Disabling reader mode");
+        Activity activity = this;//getActivity();
+        NfcAdapter nfc = NfcAdapter.getDefaultAdapter(activity);
+        if (nfc != null) {
+            nfc.disableReaderMode(activity);
+        }
+    }
+
+    @Override
+    public void onSerialNumberReceived(final String serialNumber) {
+        // This callback is run on a background thread, but updates to UI elements must be performed
+        // on the UI thread.
+        //getActivity().runOnUiThread(new Runnable() {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mSerialNumView.setText(serialNumber);
+            }
+        });
     }
 }
